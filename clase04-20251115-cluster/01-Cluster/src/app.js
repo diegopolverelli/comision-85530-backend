81c2b418fc9424a63a01030a6e8c67c3c34c55f5
Diego Polverelli <diegopolverelli@hotmail.com>
2025-11-15 14:10:53 -0300

    clase 04 added

---
+++ clase04-20251115-cluster/01-Cluster/src/app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import cluster from "cluster"
+import os from "os"
+import { router as pruebasRouter } from './routes/pruebaRouter.js';
+
+if(cluster.isPrimary){
+    console.log(`Cantidad de CPUs disponibles: `, os.cpus().length)
+    console.log(`Generando workers`)
+    for(let i=0; i<os.cpus().length; i++){
+        cluster.fork()
+    }
+    // cluster.fork()
+    // cluster.fork()
+    // cluster.fork()
+    // cluster.fork()
+    cluster.on("disconnect", worker=>{
+        console.log(`El worker ${worker.id} ha finalizado de manera inesperada; generando uno nuevo`)
+        cluster.fork()
+    })
+}else{
+    const PORT=3000;
+
+    const app=express();
+    
+    app.use(express.json());
+    app.use(express.urlencoded({extended:true}));
+    
+    app.use("/", pruebasRouter)
+    
+    let visitas=0
+    app.get('/',(req,res)=>{
+        visitas++
+        res.setHeader('Content-Type','text/plain');
+        res.status(200).send(`Visitas: ${visitas}`);
+    })
+    
+    const server=app.listen(PORT,()=>{
+        console.log(`Server escuchando en puerto ${PORT} - pid: ${process.pid} - worker id: ${cluster.worker.id}`);
+    });
+    
+}
+
+
